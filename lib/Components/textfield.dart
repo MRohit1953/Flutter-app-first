92dabb3fa70c24f695538b5be37cc445e5d192de
MRohit1953 <magarrohit1953@gmail.com>
2023-02-23 16:36:35 +0530

    added email and password fields

---
+++ lib/Components/textfield.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import '../Constants.dart' as constant;
+
+class textfield extends StatelessWidget {
+  final String text;
+  final bool isPassword;
+
+  const textfield({
+    super.key,
+    required this.text,
+    required this.isPassword,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(15.0),
+      child: TextField(
+        obscureText: isPassword,
+        decoration: InputDecoration(
+            hintText: text,
+            hintStyle: TextStyle(
+              color: constant.dayPrimary,
+              fontWeight: FontWeight.w500,
+              fontStyle: FontStyle.italic,
+              fontSize: 20.0,
+            ),
+            enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(
+                color: constant.dayPrimary,
+                width: 2,
+                style: BorderStyle.solid,
+              ),
+
+            )),
+      ),
+    );
+  }
+}
--- lib/Constants.dart
+++ lib/Constants.dart
@@ -1,3 +1,5 @@
 import 'package:flutter/material.dart';
 
 const ColortextPrimary = Color(0xff313745);
+
+const dayPrimary = Color(0xffe5ecf4);
